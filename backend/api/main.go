64bb40a669af38f78bb74d9233c12d2669fc3228
nishun <ninosekunlight@gmail.com>
2020-09-25 19:19:44 +0900

    no message

--- backend/api/main.go
+++ backend/api/main.go
@@ -1,29 +1,48 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"strings"
+    "time"
+    "github.com/gin-gonic/gin"
+    "github.com/gin-contrib/cors"
+    c "github.com/nishun0419/react-go/controllers"
 )
 
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()       //オプションを解析します。デフォルトでは解析しません。
-	fmt.Println(r.Form) //このデータはサーバのプリント情報に出力されます。
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-	}
-	fmt.Fprintf(w, "Hello astaxie!") //ここでwに入るものがクライアントに出力されます。
-}
-
 func main() {
-	http.HandleFunc("/", sayhelloName)       //アクセスのルーティングを設定します。
-	err := http.ListenAndServe(":8080", nil) //監視するポートを設定します。
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+        r := gin.Default()
+	    // r.Use() で使用したいミドルウェアなどの指定ができる。
+	    // https://godoc.org/github.com/gin-gonic/gin#RouterGroup.Use
+	    r.Use(cors.New(cors.Config{
+	        // 許可したいHTTPメソッドの一覧
+	        AllowMethods: []string{
+	            "POST",
+	            "GET",
+	            "OPTIONS",
+	            "PUT",
+	            "DELETE",
+	        },
+	        // 許可したいHTTPリクエストヘッダの一覧
+	        AllowHeaders: []string{
+	            "Access-Control-Allow-Headers",
+	            "Content-Type",
+	            "Content-Length",
+	            "Accept-Encoding",
+	            "X-CSRF-Token",
+	            "Authorization",
+	        },
+	        // 許可したいアクセス元の一覧
+	        AllowOrigins: []string{
+	            "http://localhost:3000",
+	        },
+	        // 自分で許可するしないの処理を書きたい場合は、以下のように書くこともできる
+	        // AllowOriginFunc: func(origin string) bool {
+	        //  return origin == "https://www.example.com:8080"
+	        // },
+	        // preflight requestで許可した後の接続可能時間
+	        // https://godoc.org/github.com/gin-contrib/cors#Config の中のコメントに詳細あり
+	        MaxAge: 24 * time.Hour,
+	    }))
+        r.GET("/", c.GetTodos)
+        r.POST("/todo/post", c.PostTodo)
+        r.GET("/todo/delete/:id", c.DeleteTodo)
+        r.Run(":8888") 
 }
